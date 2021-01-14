3771caf30e3132f97459ddbf8fdd6f3cd6602ed0
lohit <lohit.pereira09@stanislausbandra.in>
2021-01-14 19:00:06 +0530

    must fail

--- tests/InstagramTest.php
+++ tests/InstagramTest.php
@@ -34,7 +34,7 @@ class InstagramTest extends PHPUnit_Framework_TestCase
             "https://www.instagram.com/rits_badiani/",
             "https://www.instagram.com/vitastabhat/"
         ];
-        $this->assertCount(9,$rawHandles);
+        $this->assertCount(8,$rawHandles);
     }
 }
 
